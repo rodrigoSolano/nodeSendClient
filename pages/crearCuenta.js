38c1bb3cdd82d60c36abbe7d1cea7dba3bb4be52
rodrigoSolano <rodrigosolano865@gmail.com>
2021-12-29 22:42:42 -0600

    Crear el formulario en el componente 'crearCuenta'

--- pages/crearCuenta.js
+++ pages/crearCuenta.js
@@ -2,10 +2,70 @@ import { Layout } from "components";
 
 const CrearCuenta = () => {
   return (
-    <>
-      <Layout />
-      Crear Cuenta
-    </>
+    <Layout >
+      <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
+        <h2 className="text-4xl font-sans font-bold text-gray-800 text-center my-4">
+          Crear Cuenta
+        </h2>
+        <div className="flex justify-center mt-5">
+          <div className="w-full max-w-lg">
+            <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+              <div className="mb-4 ">
+                <label 
+                  className="block text-black text-sm font-bold mb-2"
+                  htmlFor="nombre"
+                >
+                    Nombre
+                </label>
+                <input 
+                  type="text"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="nombre"
+                  placeholder="Nombre de usuario"
+                /> 
+              </div>
+
+              <div className="mb-4 ">
+                <label 
+                  className="block text-black text-sm font-bold mb-2"
+                  htmlFor="email"
+                >
+                    Email
+                </label>
+                <input 
+                  type="email"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="email"
+                  placeholder="Nombre de usuario"
+                /> 
+              </div>
+
+              <div className="mb-4 ">
+                <label 
+                  className="block text-black text-sm font-bold mb-2"
+                  htmlFor="password"
+                >
+                    Password
+                </label>
+                <input 
+                  type="password"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  id="password'"
+                  placeholder="Nombre de usuario"
+                /> 
+              </div>
+
+              <input 
+                type="submit"
+                className="bg-red-500 hover:bg-gray-900 w-full py-2 text-white uppercase font-bold cursor-pointer"
+                value={ "Crear cuenta" }
+              />
+
+            </form>
+          </div>
+        </div>
+      </div>
+    </Layout>
   )
 }
 
