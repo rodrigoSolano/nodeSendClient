16e8539eb6d2788a0b5b7a7041754ea23cd6871d
rodrigoSolano <rodrigosolano865@gmail.com>
2021-12-29 23:34:09 -0600

    Crear el context para la autenticacion de usuarios
    * Se utilizara useReducer
    * El reducer no esta implementado
    * Se agrego el provider a la apliacion global

---
+++ context/auth/authState.js
@@ -0,0 +1,30 @@
+import React, { useReducer } from "react";
+import authContext from "./authContext";
+import authReducer from "./authReducer";
+
+const AuthState = ({ children }) => {
+
+  // Definir state inicial
+  const initialState = {
+    token: '',
+    autenticado: null,
+    usuario: null,
+    mensaje: null,
+  }
+
+  // Definir el reducer
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
+  return (
+    <authContext.Provider value={{
+      token: state.token,
+      autenticado: state.autenticado,
+      usuario: state.usuario,
+      mensaje: state.mensaje,
+    }}>
+      {children}
+    </authContext.Provider>
+  )
+}
+
+export default AuthState;
