70c43d16b417ef4faa9906c116ced29cab2992e5
rodrigoSolano <rodrigosolano865@gmail.com>
2022-01-01 14:39:16 -0600

    Mostrar el boton de crear enlace una vez se sube el archivo

--- components/Dropzone/Dropzone.js
+++ components/Dropzone/Dropzone.js
@@ -27,32 +27,49 @@ const Dropzone = () => {
     </li>
   ))
 
+  const crearEnlace = () => {
+    console.log("Creando enlace...")
+  }
+
   return (
     <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0 flex flex-col items-center justify-center border-dashed border-gray-400 border-2 bg-gray-100 px-4">
 
-      <ul>
-        {archivos}
-      </ul>
+      {acceptedFiles.length > 0 ? (
+        <div className="mt-10 w-full">
+          <h4 className="text-2xl font-bold text-center mb-4">Archivos</h4>
+          <ul>
+            {archivos}
+          </ul>
+          <button
+            type="button"
+            className="bg-blue-700 w-full py-3 rounded-lg text-white my-10 hover:bg-blue-800"
+            onClick={() => crearEnlace()}
+          >
+            Crear enlace
+          </button>
+        </div>
+      ) : (
+        <div {...getRootProps({ classsName: "dropzone w-full py-32" })}>
+          <input className="h-100" {...getInputProps()} />
+          {
+            isDragActive
+              ?
+              <p className="text-2xl text-center text-gray-600">Suelta el archivo </p>
+              :
+              (
+                <div className="text-center">
+                  <p className="text-2xl text-center text-gray-600">
+                    Selecciona un archivo y arrastralo aqui
+                  </p>
+                  <button className="bg-blue-700 w-full py-3 rounded-lg text-white my-10 hover:bg-blue-800" type="button">
+                    Selecciona archivos para subir
+                  </button>
+                </div>
+              )
+          }
+        </div>
+      )}
 
-      <div {...getRootProps({ classsName: "dropzone w-full py-32" })}>
-        <input className="h-100" {...getInputProps()} />
-        {
-          isDragActive
-            ?
-            <p className="text-2xl text-center text-gray-600">Suelta el archivo </p>
-            :
-            (
-              <div className="text-center">
-                <p className="text-2xl text-center text-gray-600">
-                  Selecciona un archivo y arrastralo aqui
-                </p>
-                <button className="bg-blue-700 w-full py-3 rounded-lg text-white my-10 hover:bg-blue-800" type="button">
-                  Selecciona archivos para subir
-                </button>
-              </div>
-            )
-        }
-      </div>
     </div>
   )
 }
