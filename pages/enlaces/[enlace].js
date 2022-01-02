354b359efa1a21b789409921d2a31f2c427e122c
rodrigoSolano <rodrigosolano865@gmail.com>
2022-01-01 18:50:12 -0600

    Descargar archivos alojados en el servidor

--- pages/enlaces/[enlace].js
+++ pages/enlaces/[enlace].js
@@ -23,10 +23,21 @@ export async function getServerSidePaths() {
 }
 
 const Enlace = ({ enlace }) => {
-  console.log("Enlace: ",enlace);
+  console.log("Enlace: ", enlace);
   return (
     <Layout>
-      Desde enlace JS
+      <h1 className="text-4xl text-center text-gray-700">
+        Descarga tu archivo
+        <div className="flex items-center justify-center mt-10">
+          <a
+            href={`${process.env.API_URL}/api/archivos/${enlace.archivo}`}
+            className="bg-red-500 text-center px-10 py-3 rounden uppercase font-bold text-white cursor-pointer"
+            download
+          >
+            Aqui
+          </a>
+        </div>
+      </h1>
     </Layout>
   )
 }
